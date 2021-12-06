e5f0afb5980020e2459b7d44a07996b1d37d8728
junbeom-kang <kgood10369@gmail.com>
2021-12-06 22:26:57 +0900

    no message

--- src/model/거주공간모델.py
+++ src/model/거주공간모델.py
@@ -1,10 +1,8 @@
 from keras import models
 from keras import layers
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
-from math import ceil
 
 from sklearn.model_selection import train_test_split
 
@@ -21,37 +19,30 @@ def build_model():
     return model
 
 
-df = pd.read_csv('C:/User/data123.csv',encoding='cp949')
-UV = df['UV index']
+df = pd.read_csv('C:/User/indata123.csv',encoding='cp949')
+UV_sensor = df['uv_sensor']//10
+Volts=df['volts']
 
 
-
-df = df[['Aerosol','Temp_avg','Temp_min','Temp_max', 'Humidity','sunshine time','cloud cover']]
+df = df[['Temp_avg','Temp_min','Temp_max', 'Humidity','cloud cover','uv index']]
 x = df[:]
-UV_X_train, UV_X_test, UV_Y_train, UY_Y_test = train_test_split(df, UV, test_size=0.2, shuffle=True, random_state=1234)
+UV_sensor_train, UV_sensor_test, UV_sensor_Y_train, UV_sensor_Y_test = train_test_split(df, UV_sensor, test_size=0.2, shuffle=True, random_state=1234)
+UV_sensorModel = build_model()
+UV_sensor_history = UV_sensorModel.fit(UV_sensor_train, UV_sensor_Y_train, epochs=300, batch_size=10, verbose=2)
 
-CUVModel = build_model()
-MUVModel = build_model()
-
-CUV_history = CUVModel.fit(UV_X_train, UV_Y_train, epochs=100, batch_size=10, verbose=2)
-MUV_history = MUVModel.fit(UV_X_train, UV_Y_train, epochs=100, batch_size=10, verbose=2)
-
-CUV_hat = CUVModel.predict(UV_X_test)
-MUV_hat = MUVModel.predict(UV_X_test)
-
-
+UV_sensor_hat = UV_sensorModel.predict(UV_sensor_test)
+print(mean_squared_error(UV_sensor_hat, UV_sensor_Y_test))
+print((mean_squared_error(UV_sensor_hat, UV_sensor_Y_test))**0.5)
+"""
 plt.figure(figsize=(12,4))
 plt.suptitle('MSE according to epoch ',fontsize=20)
-plt.subplot(1,1,1)
+plt.subplot(1,2,1)
 plt.ylabel('mse')
 plt.xlabel('epoch')
 plt.title('CUV', fontsize=10)
-plt.plot(CUV_history.history['loss'])
-
+plt.plot(UV_sensor_history.history['loss'])
 plt.show()
-print(mean_squared_error(CUV_hat, UY_Y_test))
-
-
+"""
 
 
 
